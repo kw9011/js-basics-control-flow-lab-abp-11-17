a995db772d202ce8d49400745c156c10393244a3
Karmy Widjaja <karmy0311@gmail.com>
2017-11-11 15:59:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ function scuberGreetingForFeet(feet) {
   if(feet<=400){
     return 'This one is on me!'
   }
-
+else if (1999 < feet && feet <2500){
+  return "I will gladly take your thirty bucks."
+}
