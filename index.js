3e8725baf9dfe4a1f896cc4124c46684fabc4701
Karmy Widjaja <karmy0311@gmail.com>
2017-11-11 16:31:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,4 @@ function ternaryCheckCity(city) {
 }
 
 
-function switchOnCharmFromTip(tip) {
-  switch (tip) {
-    case 'generous':
-  return'Thank you so much.'
-  break;
-  case'not as generous':
-  return'Thank you.'
-  default:
-  return'Bye.'
-}
+
