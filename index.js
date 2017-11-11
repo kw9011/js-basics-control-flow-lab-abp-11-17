6b1d8bca9b0981096c79830f77359fa496cf3542
Karmy Widjaja <karmy0311@gmail.com>
2017-11-11 16:30:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ function scuberGreetingForFeet(number) {
   else if (number > 2500)
     return 'No can do.';
 }
-let city 
+let city
 function ternaryCheckCity(city) {
   return city === 'NYC'? 'Ok, sounds good.' : 'No go.'
+}
+let tip 
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+  return'Thank you so much.'
+  break;
+  case'not as generous':
+  return'Thank you.'
+  default: 
+  return'Bye.'
 }
