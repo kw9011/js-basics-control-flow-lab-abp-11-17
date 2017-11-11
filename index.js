dc451c8d600036a3828d50d9af17d2a0b2ef9430
Karmy Widjaja <karmy0311@gmail.com>
2017-11-11 16:31:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ function ternaryCheckCity(city) {
   return city === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
+let tip
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+  return'Thank you so much.'
+  break;
+  case'not as generous':
+  return'Thank you.'
+  default:
+  return'Bye.'
+}
