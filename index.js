8c28bb2c38cc67c1c5f5887ebaea71e3384a4e2e
Karmy Widjaja <karmy0311@gmail.com>
2017-11-11 16:04:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Write your code in this file!
-function scuberGreetingForFeet(feet) {
-  if(feet<=400){
-    return 'free sample'
-  
+function scuberGreetingForFeet (feet){
+  if(feet <=400){
+    return 'tHis one is on me!'
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
