59dd1788f33ab0f839e0761564adf958ef994fd8
Karmy Widjaja <karmy0311@gmail.com>
2017-11-11 16:13:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // Write your code in this file!
-let number 
-function scuberGreetingForFeet(number)
+let number
+function scuberGreetingForFeet(number) {
+  if (number <= 400)
+   return "This one is on me!";
+  else if (number >= 2000 && number <= 2500)
+    return 'I will gladly take your thirty bucks.';
+  else if (number > 2500)
+    return 'No can do.';
+}
