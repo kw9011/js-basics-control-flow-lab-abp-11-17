64327dded6a2c91a52b8fc69d76a55405e811daa
Karmy Widjaja <karmy0311@gmail.com>
2017-11-11 16:13:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 // Write your code in this file!
-let number
-function scuberGreetingForFeet(number){
-  if number <=400) 
-}
- -
- -function ternaryCheckCity(city){
- -  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
- -}
- -
- -function switchOnCharmFromTip(tip){
- -  switch (tip) {
- -    case 'generous':
- -      return 'Thank you so much.'
- -      break;
- -    case 'not as generous':
- -      return 'Thank you.'
- -    default:
- -      return 'Bye.'
- -  }
- -}
+let number 
+function scuberGreetingForFeet(number)
